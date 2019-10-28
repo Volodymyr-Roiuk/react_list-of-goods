57d2507cc605bf68a3d659e0f3ddb5f5115c6563
Volodymyr_Roiuk <heppivova@gmail.com>
2019-10-28 15:47:39 +0300

    Updates

---
+++ precache-manifest.dd02f4e394dbad680858497008c0a417.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00b59bfbe67ee27747da30f9841d4a05",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "6d4b6615bbc33e17cb18",
+    "url": "/react_list-of-goods/static/css/main.28d82bef.chunk.css"
+  },
+  {
+    "revision": "1e9d4a1cd97a0f8b7133",
+    "url": "/react_list-of-goods/static/js/2.f32803e1.chunk.js"
+  },
+  {
+    "revision": "6d4b6615bbc33e17cb18",
+    "url": "/react_list-of-goods/static/js/main.8a141333.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
